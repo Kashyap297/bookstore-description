2fb6a458551f6c6b996ba4c8ee4c41d17429fae6
Kashyap297 <kashyap29700@gmail.com>
2024-03-04 00:12:50 +0530

    read, add, delete done

--- bookstore/server.js
+++ bookstore/server.js
@@ -1,12 +1,86 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/bookStore')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const productSchema = new mongoose.Schema({
+    bookid: {
+        type: Number,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    booktype: {
+        type: String,
+        required: true,
+    },
+    stock: {
+        type: Number,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    publishdate: {
+        type: Date,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: true,
+    }
+
+}, { timestamps: true })
+
+const productModel = mongoose.model('Book', productSchema)
 
 app.set('view engine', 'ejs')
 
+// home
 app.get('/', (req, res) => {
     res.render('./Pages/home')
 })
 
-app.listen(9000, ()=>{
+// retrive Data
+app.get('/booklist', async (req, res) => {
+    const books = await productModel.find()
+    res.render('./Pages/index', { books: books })
+})
+
+// add data
+app.get('/addbook', (req, res) => {
+    res.render('./Pages/addbook')
+})
+
+// post data
+app.post('/addbook', async (req, res) => {
+    const book = req.body;
+
+    const newBook = new productModel(book);
+    await newBook.save();
+
+    res.redirect('/booklist')
+})
+
+
+// deleteData
+app.get('/deleteBook/:id', async (req, res) => {
+    const userId = req.params.id;
+    var result = await productModel.deleteOne(({ _id: userId }))
+    res.redirect('/booklist')
+})
+
+// edit Data
+
+
+// port Listen at
+app.listen(9000, () => {
     console.log('Server Start');
 })
